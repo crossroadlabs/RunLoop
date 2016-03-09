fe11c055e19645c87dec3275d0798a2047b47e84
Daniel Leping <dileping@gmail.com>
2016-03-09 10:29:10 +0200

    conditional availability of DispatchRunLoop

--- RunLoop/DispatchRunLoop.swift
+++ RunLoop/DispatchRunLoop.swift
@@ -14,93 +14,95 @@
 //limitations under the License.
 //===----------------------------------------------------------------------===//
 
-import Foundation
-import Dispatch
-
-import Boilerplate
-
-public class DispatchSemaphore : SemaphoreType {
-    let sema:dispatch_semaphore_t
+#if !os(Linux) || dispatch
+    import Foundation
+    import Dispatch
     
-    public required convenience init() {
-        self.init(value: 0)
+    import Boilerplate
+    
+    public class DispatchSemaphore : SemaphoreType {
+        let sema:dispatch_semaphore_t
+        
+        public required convenience init() {
+            self.init(value: 0)
+        }
+        
+        public required init(value: Int) {
+            self.sema = dispatch_semaphore_create(value)
+        }
+        
+        public func wait() -> Bool {
+            return wait(.Infinity)
+        }
+        
+        public func wait(until:NSDate) -> Bool {
+            return wait(Timeout(until: until))
+        }
+        
+        public func wait(timeout: Timeout) -> Bool {
+            let time = timeout.dispatchTime
+            let result = dispatch_semaphore_wait(sema, time)
+            return result == 0
+        }
+        
+        public func signal() -> Int {
+            return dispatch_semaphore_signal(sema)
+        }
     }
     
-    public required init(value: Int) {
-        self.sema = dispatch_semaphore_create(value)
-    }
-    
-    public func wait() -> Bool {
-        return wait(.Infinity)
-    }
-    
-    public func wait(until:NSDate) -> Bool {
-        return wait(Timeout(until: until))
-    }
-    
-    public func wait(timeout: Timeout) -> Bool {
-        let time = timeout.dispatchTime
-        let result = dispatch_semaphore_wait(sema, time)
-        return result == 0
-    }
-    
-    public func signal() -> Int {
-        return dispatch_semaphore_signal(sema)
-    }
-}
-
-public class DispatchRunLoop: RunLoopType, NonStrictEquatable {
-    private let _queue:dispatch_queue_t!
-    
-    init(queue:dispatch_queue_t!) {
-        self._queue = queue
-    }
-    
-    public required convenience init() {
-        let name = NSUUID().UUIDString
-        let queue = dispatch_queue_create(name, nil)
-        self.init(queue: queue)
-    }
-    
-    public func semaphore() -> SemaphoreType {
-        return DispatchSemaphore()
-    }
-    
-    public func semaphore(value:Int) -> SemaphoreType {
-        return DispatchSemaphore(value: value)
-    }
-    
-    public func execute(task:SafeTask) {
-        dispatch_async(_queue) {
-            RunLoop.trySetFactory {
-                return self
+    public class DispatchRunLoop: RunLoopType, NonStrictEquatable {
+        private let _queue:dispatch_queue_t!
+        
+        init(queue:dispatch_queue_t!) {
+            self._queue = queue
+        }
+        
+        public required convenience init() {
+            let name = NSUUID().UUIDString
+            let queue = dispatch_queue_create(name, nil)
+            self.init(queue: queue)
+        }
+        
+        public func semaphore() -> SemaphoreType {
+            return DispatchSemaphore()
+        }
+        
+        public func semaphore(value:Int) -> SemaphoreType {
+            return DispatchSemaphore(value: value)
+        }
+        
+        public func execute(task:SafeTask) {
+            dispatch_async(_queue) {
+                RunLoop.trySetFactory {
+                    return self
+                }
+                task()
             }
-            task()
         }
-    }
-    
-    public func execute(delay:Timeout, task:SafeTask) {
-        dispatch_after(delay.dispatchTime, _queue) {
-            RunLoop.trySetFactory {
-                return self
+        
+        public func execute(delay:Timeout, task:SafeTask) {
+            dispatch_after(delay.dispatchTime, _queue) {
+                RunLoop.trySetFactory {
+                    return self
+                }
+                task()
             }
-            task()
+        }
+        
+        public var native:Any {
+            get {
+                return _queue
+            }
+        }
+        
+        public static let main:RunLoopType = DispatchRunLoop(queue: dispatch_get_main_queue())
+        
+        public func isEqualTo(other: NonStrictEquatable) -> Bool {
+            guard let other = other as? DispatchRunLoop else {
+                return false
+            }
+            //TODO: test
+            return dispatch_queue_get_label(self._queue) == dispatch_queue_get_label(other._queue)
         }
     }
-    
-    public var native:Any {
-        get {
-            return _queue
-        }
-    }
-    
-    public static let main:RunLoopType = DispatchRunLoop(queue: dispatch_get_main_queue())
-    
-    public func isEqualTo(other: NonStrictEquatable) -> Bool {
-        guard let other = other as? DispatchRunLoop else {
-            return false
-        }
-        //TODO: test
-        return dispatch_queue_get_label(self._queue) == dispatch_queue_get_label(other._queue)
-    }
-}
+#endif
