8aa9bc1176146efe2a772f6e3f743a29564f7e5a
Yehor Popovych <y.popovych@crossroadlabs.xyz>
2016-04-08 16:40:07 +0300

    Dependencies

--- Package.swift
+++ Package.swift
@@ -19,5 +19,9 @@ import PackageDescription
 let package = Package(
     name: "RunLoop",
     targets: [Target(name: "RunLoop")],
-    dependencies: [.Package(url: "https://github.com/crossroadlabs/UV.git", majorVersion: 0)]
+    dependencies: [
+        .Package(url: "https://github.com/crossroadlabs/UV.git", majorVersion: 0),
+        .Package(url: "https://github.com/crossroadlabs/Foundation3.git", majorVersion: 0),
+        .Package(url: "https://github.com/crossroadlabs/XCTest3.git", majorVersion: 0)
+    ]
 )
